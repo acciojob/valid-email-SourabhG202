e4d733b5705e2459e14525221c3ccc25408aa437
Sourabh G <154436409+SourabhG202@users.noreply.github.com>
2025-04-06 10:53:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function validEmail(str) {
   //your JS code here.
+	 if (!str) return false;
+
+  // Yeh regex check karega ki email format sahi hai ya nahi
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  // Regex ke basis par true ya false return karo
+  return emailRegex.test(str);
+	}
+	
 }
 
 // Do not change the code below.
